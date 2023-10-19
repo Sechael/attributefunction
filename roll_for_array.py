34cc598e106b0be5a2aefd9dd3e6efe9ce11afe1
Sechael <smsuepple@web.de>
2023-10-19 22:12:59 +0200

    Changed code to executeable

---
+++ roll_for_array.py
@@ -0,0 +1,60 @@
+import random
+import csv
+import math
+
+import tkinter as tk
+from tkinter import simpledialog
+
+# Create a tkinter window
+root = tk.Tk()
+root.geometry("300x200")
+
+# Prompt the user for input using tkinter's simpledialog
+max_score = simpledialog.askinteger("Input", "Enter the maximum possible score (e.g., 15):")
+min_score = simpledialog.askinteger("Input", "Enter the minimum possible score (e.g., 3):")
+point_pool = simpledialog.askinteger("Input", "Enter the point pool (e.g., 65):")
+X = simpledialog.askinteger("Input", "Enter the number of rows to generate:")
+
+def init_array(X):
+    array = []
+    for _ in range(X):
+        # Generate a list with 7 random values that sum to point_pool
+        row = random.sample(range(min_score, max_score + 1), 7)
+        # Calculate the remaining points
+        remaining_points = point_pool - sum(row)
+        # Add the remaining points randomly to the existing values
+        while remaining_points > 0:
+            index = random.randint(0, 6)
+            if row[index] < max_score:
+                row[index] += 1
+                remaining_points -= 1
+        # Check if the sum exceeds point_pool and subtract points if needed
+        while sum(row) > point_pool:
+            index = random.randint(0, 6)
+            if row[index] > min_score:
+                row[index] -= 1
+        # Calculate the average of Dexterity and Intelligence
+        average_dex_int = (row[1] + row[3]) // 2
+        # Add the average as the 8th column
+        row.append(average_dex_int)
+        array.append(row)
+    return array
+
+array = init_array(X)
+
+root.destroy()
+
+# Define the CSV filename
+csv_filename = 'output.csv'
+
+# Define column labels
+column_labels = ["STR", "DEX", "CON", "INT", "PERC", "WILL", "CHA", "Initative"]
+
+# Write the data to the CSV file with column labels
+with open(csv_filename, 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["Row"] + column_labels)
+    for i, row in enumerate(array):
+        csv_writer.writerow([i + 1] + row)
+
+print(f"Data for {X} rows has been written to {csv_filename}.")
